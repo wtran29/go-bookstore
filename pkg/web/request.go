96a1a0bb2e3a2a082cf8f08dbb5e8d72eebb3c2a
William Tran <wtran29@gmail.com>
2023-02-07 20:11:10 -0800

    created a utility function to decode and parse json

---
+++ pkg/web/request.go
@@ -0,0 +1,16 @@
+package web
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+func Decode(r *http.Request, val any) error {
+	decoder := json.NewDecoder(r.Body)
+	decoder.DisallowUnknownFields()
+	if err := decoder.Decode(val); err != nil {
+		return err
+	}
+
+	return nil
+}
